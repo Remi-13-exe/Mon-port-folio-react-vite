51c85dab2dd32bcb46ecbe9af56373502034bd3b
Remi-13-exe <rgodji16@gmail.com>
2025-10-05 11:58:08 +0200

    finition du projet

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import history from 'connect-history-api-fallback';
 
 export default defineConfig({
   plugins: [react()],
-  
-  // Base pour GitHub Pages (change 'Mon-port-folio-react-vite' par ton repo exact)
-  base: '/Mon-port-folio-react-vite/',
-
-  server: {
-    middlewareMode: false,
-    setupMiddlewares(middlewares) {
-      // Permet le routing React sur Vite dev
-      middlewares.use(
-        history({
-          index: '/index.html',
-        })
-      );
-      return middlewares;
-    },
-  },
+  base: '/Mon-port-folio-react-vite/',  // <-- Nom exact du repo
 });
